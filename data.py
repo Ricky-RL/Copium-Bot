aee8b19ad959bff43f3c8f987ac727a1b3c824f7
Ricky Lin <rickylin543@gmail.com>
2024-12-27 15:26:53 -0800

    update help

--- data.py
+++ data.py
@@ -2,25 +2,26 @@ allchamp = ["Aatrox","Ahri","Akali","Alistar","Amumu","Anivia","Annie","Aphelios
 people = ["iplaygam", "epickc123", "masterfireking", "warscout101", "jaycsee", "iplaygamv2", "mann.h"]
 people_test = ['iplaygam', 'iplaygamv2']
 help_str = """
-**Bot Commands**
+**PLAY GAMES**
+- `!play [profile_name]`: Sends a request to play the profile-specific game to its members.
+
+**PROFILE MANAGEMENT COMMANDS**
 - `!get_status`: Returns the status of the server.
 - `!get_profiles`: Prints out a list of all profiles.
-- `!play [profile_name]`: Sends a request to play the profile-specific game to its members.
 - `!create_profile [profile_name] [member_name1] [member_name2] ...`: Adds all members to the profile. 
    - Example: `!create_profile hearts of iron iv iplaygam iplaygamv2`
 - `!delete_profile [profile_name]`: Deletes the profile with the given name.
 
-**Player Interaction Commands**
+**ANNOY PEOPLE COMMANDS**
 - `?nudge [discord_tag_1] [discord_tag_2] ...`: nudges the player(s) specified in the tag to hop on. Requires at least one tag in input. Run `?tags` to see valid player tags
-- `?tags`: See the list of all valid Discord tags.
+- `?remind`: Send a reminder to all players that have yet to respond.
+
+**RESPONSE COMMANDS**
 - `?y`: Confirm that you are playing tonight.
 - `?n`: Confirm that you are not playing tonight.
 - `?time in minute(s)`: Request a delay of the specified length.
    - Example: `?10`
-- `?list`: View the people who have already responded.
-- `?recommend`: Proprietary trained deep neural net model custom GPT ML algorithm that recommends champions based on projected player performance and meta shifts.
 - `?clear`: Clears the list of players that are coming to play.
-- `?remind`: Send a reminder to all players that have yet to respond.
 
 ** Communication Commands**
 - `*{OPTIONAL: profile_name} any text`: Send a message to all users.
@@ -28,7 +29,13 @@ help_str = """
     - Example: `*test` sends the message `test` to all users
 
 **Server Management Commands**
-- `start_server`: Start up the Valheim server.
+- `!start_server {profile_name}`: Start up specific server to profile.
    - **Note:** *MAKE SURE TO SHUT DOWN THE SERVER AFTERWARDS.*
-- `stop_server`: Stop the Valheim server.
+- `!stop_server {profiile_name`: Stop specific server to profle.
+- `!get_status {profile_name}`: Get information on specific server to profile.
+
+**MISCELLANEOUS COMMANDS**
+- `?tags`: See the list of all valid Discord tags.
+- `?recommend`: Proprietary trained deep neural net model custom GPT ML algorithm that recommends champions based on projected player performance and meta shifts.
+- `?list`: View the people who have already responded.
 """
